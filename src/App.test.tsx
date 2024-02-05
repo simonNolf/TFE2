0163c305d8b1a6660206ab6d43d4908d4d620ac3
simonNolf <simon.nolf@gmail.com>
2024-02-05 16:12:45 +0100

    test ok

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Tab1 from './pages/Tab1';
 
 test('renders without crashing', () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
 });
+
+test('rendrer bonjour Simon', async () => {
+  const { findByText } = render (<Tab1 />);
+  await findByText('Bonjour Simon')
+})
