bf646e641a3deaa78ea1e6985736e44113a48895
simonNolf <simon.nolf@gmail.com>
2024-03-27 17:26:52 +0100

    ok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import legacy from '@vitejs/plugin-legacy'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    legacy()
   ],
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit as needed
-  },
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  }
+})
